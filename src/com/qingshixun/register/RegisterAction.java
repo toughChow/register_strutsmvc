ebd987e05eac9eaec03bc1a46058cf6e3e56e747
toughChow <toughChow@gmail.com>
2017-09-05 16:45:37 +0800

    添加struts.xml的通配方法

--- src/com/qingshixun/register/RegisterAction.java
+++ src/com/qingshixun/register/RegisterAction.java
@@ -6,19 +6,21 @@ import com.opensymphony.xwork2.ModelDriven;
 public class RegisterAction extends ActionSupport implements ModelDriven<User> {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private User user = new User();
+
+	// 跳转到成功页面
+	public String success() {
+		return SUCCESS;
+	}
+
+	// 跳转到注册页面
+	public String regist() {
+		return SUCCESS;
+	}
 
 	public void setUser(User user) {
 		this.user = user;
-	}
-
-	public String register() {
-		return SUCCESS;
-	}
-
-	public String regist() {
-		return SUCCESS;
 	}
 
 	public User getModel() {
