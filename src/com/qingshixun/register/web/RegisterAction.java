f900b371de192a49ea70cbd489510e8a0129f593
toughChow <toughChow@gmail.com>
2017-09-06 09:47:47 +0800

    实现dao

--- src/com/qingshixun/register/web/RegisterAction.java
+++ src/com/qingshixun/register/web/RegisterAction.java
@@ -1,16 +1,22 @@
-package com.qingshixun.register;
+package com.qingshixun.register.web;
 
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
+import com.qingshixun.register.domain.User;
+import com.qingshixun.register.service.UserService;
+import com.qingshixun.register.service.impl.UserServiceImpl;
 
 public class RegisterAction extends ActionSupport implements ModelDriven<User> {
 
 	private static final long serialVersionUID = 1L;
 
+	private UserService userService = new UserServiceImpl();
+
 	private User user = new User();
 
 	// 跳转到成功页面
 	public String success() {
+		userService.insertUser(user);
 		return SUCCESS;
 	}
 
